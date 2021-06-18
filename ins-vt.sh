f1dd626c057eee31fb53b39c889883a8a6e0b3ff
yolawido <79554776+yolawido@users.noreply.github.com>
2021-06-18 18:05:30 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -13,7 +13,7 @@ chronyc sourcestats -v
 chronyc tracking -v
 date
 # install v2ray
-wget https://raw.githubusercontent.com/di2nk/v2/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/yolawido/v2/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
@@ -506,25 +506,25 @@ systemctl start v2ray@trojan.service
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O menu "https://raw.githubusercontent.com/di2nk/v2/main/menu.sh"
-wget -O addws "https://raw.githubusercontent.com/di2nk/v2/main/addws.sh"
-wget -O addvless "https://raw.githubusercontent.com/di2nk/v2/main/addvless.sh"
-wget -O addtr "https://raw.githubusercontent.com/di2nk/v2/main/addtr.sh"
-wget -O delws "https://raw.githubusercontent.com/di2nk/v2/main/delws.sh"
-wget -O delvless "https://raw.githubusercontent.com/di2nk/v2/main/delvless.sh"
-wget -O deltr "https://raw.githubusercontent.com/di2nk/v2/main/deltr.sh"
-wget -O cekws "https://raw.githubusercontent.com/di2nk/v2/main/cekws.sh"
-wget -O cekvless "https://raw.githubusercontent.com/di2nk/v2/main/cekvless.sh"
-wget -O cektr "https://raw.githubusercontent.com/di2nk/v2/main/cektr.sh"
-wget -O renewws "https://raw.githubusercontent.com/di2nk/v2/main/renewws.sh"
-wget -O renewvless "https://raw.githubusercontent.com/di2nk/v2/main/renewvless.sh"
-wget -O renewtr "https://raw.githubusercontent.com/di2nk/v2/main/renewtr.sh"
-wget -O xp-ws "https://raw.githubusercontent.com/di2nk/v2/main/xp-ws.sh"
-wget -O xp-tr "https://raw.githubusercontent.com/di2nk/v2/main/xp-tr.sh"
-wget -O xp-vless "https://raw.githubusercontent.com/di2nk/v2/main/xp-vless.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/di2nk/v2/main/cert.sh"
-wget -O about "https://raw.githubusercontent.com/di2nk/v2/main/about.sh"
-wget -O addhost "https://raw.githubusercontent.com/di2nk/v2/main/addhost.sh"
+wget -O menu "https://raw.githubusercontent.com/yolawido/v2/main/menu.sh"
+wget -O addws "https://raw.githubusercontent.com/yolawido/v2/main/addws.sh"
+wget -O addvless "https://raw.githubusercontent.com/yolawido/v2/main/addvless.sh"
+wget -O addtr "https://raw.githubusercontent.com/yolawido/v2/main/addtr.sh"
+wget -O delws "https://raw.githubusercontent.com/yolawido/v2/main/delws.sh"
+wget -O delvless "https://raw.githubusercontent.com/yolawido/v2/main/delvless.sh"
+wget -O deltr "https://raw.githubusercontent.com/yolawido/v2/main/deltr.sh"
+wget -O cekws "https://raw.githubusercontent.com/yolawido/v2/main/cekws.sh"
+wget -O cekvless "https://raw.githubusercontent.com/yolawido/v2/main/cekvless.sh"
+wget -O cektr "https://raw.githubusercontent.com/yolawido/v2/main/cektr.sh"
+wget -O renewws "https://raw.githubusercontent.com/yolawido/v2/main/renewws.sh"
+wget -O renewvless "https://raw.githubusercontent.com/yolawido/v2/main/renewvless.sh"
+wget -O renewtr "https://raw.githubusercontent.com/yolawido/v2/main/renewtr.sh"
+wget -O xp-ws "https://raw.githubusercontent.com/yolawido/v2/main/xp-ws.sh"
+wget -O xp-tr "https://raw.githubusercontent.com/yolawido/v2/main/xp-tr.sh"
+wget -O xp-vless "https://raw.githubusercontent.com/yolawido/v2/main/xp-vless.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/yolawido/v2/main/cert.sh"
+wget -O about "https://raw.githubusercontent.com/yolawido/v2/main/about.sh"
+wget -O addhost "https://raw.githubusercontent.com/yolawido/v2/main/addhost.sh"
 chmod +x menu
 chmod +x addws
 chmod +x addvless
